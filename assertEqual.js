46a2f64e48181f70d5b2d3401df80ed23bdfb7df
jon-jh <hiebert456@gmail.com>
2024-06-15 15:55:52 -0600

    changed console.logs to use template strings () with backticks  instead of the normal way of adding + name + etc.

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,16 @@
 // This is a function that checks if values are equal, but unlike the assert.equal built in function, we can add out own message for pass and fail.
+// Removed " + actual + " replaced with backticks `` and ${actual}
 
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log("💔 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`💔 Assertion Failed: ${actual} !== ${expected}`);
   } else {
-    console.log("💚 Assertion Passed: " + actual + " === " + expected);
+    console.log(`💚 Assertion Passed: ${actual} === ${expected}`);
   }
-  return actual === expected;
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("zero", 0);
+assertEqual(79, 79);
+assertEqual("password", "password");
