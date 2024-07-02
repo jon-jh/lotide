03dfbcf64a2c03b716c6ddf3aed2ff76cad9650a
jon-jh <hiebert456@gmail.com>
2024-07-02 13:36:43 -0600

    refactor to use module exports instead of copy pasting original code all over

--- assertEqual.js
+++ assertEqual.js
@@ -1,17 +1,10 @@
-// This is a function that checks if values are equal, but unlike the assert.equal built in function, we can add out own message for pass and fail.
-// Removed " + actual + " replaced with backticks `` and ${actual}
-
-// assertEqual checks that the values in the function are equal.
+// This is a function that checks if values are equal.
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log(`💔assertEqual found: false: ${actual} !== ${expected}`);
+    console.log(`aE found ⛔ ${actual} !== ${expected}`);
   } else {
-    console.log(`💚assertEqual found: true: ${actual} === ${expected}`);
+    console.log(`aE found 🟢 ${actual} === ${expected}`);
   }
 };
-// assertEqual("test", "test");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual("zero", 0);
-assertEqual(79, 79);
-assertEqual("password", "password");
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,18 +1,7 @@
-// assertEqual checks that the values in the function are equal.
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`💔assertEqual found: false: ${actual} !== ${expected}`);
-  } else {
-    console.log(`💚assertEqual found: true: ${actual} === ${expected}`);
-  }
-};
-// assertEqual("test", "test");
-// -----
-// -----
 // Create a function "head" which returns the first item in the array.
 const head = function(array) {
+  console.log("[head]");
   return array[0];
 };
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hi", "Test"]), "Hello");
-assertEqual(head(["Hey", "Test"]), "Hey");
+
+module.exports = head;
